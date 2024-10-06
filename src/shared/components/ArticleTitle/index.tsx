421525303ffdaf22f97b1c69946aafa76b75ee6b
ChiVi123 <nguyenhoangchivi@gmail.com>
2024-10-06 14:02:10 +0700

    refactor(article title): add tag and date

--- src/shared/components/ArticleTitle/index.tsx
+++ src/shared/components/ArticleTitle/index.tsx
@@ -1,21 +1,38 @@
 import Title from 'antd/es/typography/Title';
 import classNames from 'classnames/bind';
 
+import { ClockIcon, TagIcon } from '~icons';
 import styles from './article-title.module.scss';
 
 interface IProps {
     title: string;
     description: string;
+    category?: string;
+    date?: string;
 }
 
 const cx = classNames.bind(styles);
 
-function ArticleTitle({ title, description }: IProps) {
+function ArticleTitle({ title, description, category, date }: IProps) {
     return (
         <div className={cx('wrap')}>
-            <Title level={1} role='heading' className={cx('title')}>
-                {title}
-            </Title>
+            <div>
+                <Title level={1} role='heading' className={cx('title')}>
+                    {title}
+                </Title>
+                <div className={cx('sub-title')}>
+                    {category && (
+                        <span className={cx('sub-title-tag')}>
+                            <TagIcon /> {category}
+                        </span>
+                    )}
+                    {date && (
+                        <span className={cx('sub-title-date')}>
+                            <ClockIcon /> {date}
+                        </span>
+                    )}
+                </div>
+            </div>
             <div className={cx('description-wrap')}>
                 <span className={cx('description-title')}>Mô tả</span>
                 <p className={cx('description-content')}>{description}</p>
