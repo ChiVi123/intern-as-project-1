0c0a2c508f30981647133ecc2d453c5dbfcec654
ChiVi123 <nguyenhoangchivi@gmail.com>
2024-10-08 13:51:22 +0700

    add introduce staff page

--- src/shared/components/index.tsx
+++ src/shared/components/index.tsx
@@ -1,7 +1,9 @@
 export { default as ArticleThumbnail } from './ArticleThumbnail';
 export { default as ArticleTitle } from './ArticleTitle';
 export { default as Card } from './Card';
+export { default as CarouselThumbnail } from './CarouselThumbnail';
 
 export * from './ButtonMore';
+export * from './CarouselThumbnail';
 export * from './MenuTransitionGroup';
 export * from './Slot';
