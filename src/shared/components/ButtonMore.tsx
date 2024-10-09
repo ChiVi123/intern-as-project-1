b17899293476f5ffe41a9ae868e9df8b90a8a53d
ChiVi123 <nguyenhoangchivi@gmail.com>
2024-10-09 11:42:25 +0700

    refactor(ButtonMore): class name

--- src/shared/components/ButtonMore.tsx
+++ src/shared/components/ButtonMore.tsx
@@ -6,15 +6,17 @@ interface IProps extends ButtonProps {
     color?: 'green' | 'pink';
 }
 
-export const ButtonMore = forwardRef<HTMLButtonElement, IProps>(({ color = 'green', ...buttonProps }, ref) => (
-    <Button
-        ref={ref}
-        iconPosition='end'
-        icon={<ArrowNarrowLeftIcon className='rotate-180' />}
-        className={`custom-btn-more custom-btn-more--${color}`}
-        {...buttonProps}
-    >
-        Xem thêm
-    </Button>
-));
+export const ButtonMore = forwardRef<HTMLButtonElement, IProps>(
+    ({ color = 'green', className, ...buttonProps }, ref) => (
+        <Button
+            ref={ref}
+            iconPosition='end'
+            icon={<ArrowNarrowLeftIcon className='rotate-180' />}
+            className={`custom-btn-more custom-btn-more--${color} ${className}`}
+            {...buttonProps}
+        >
+            Xem thêm
+        </Button>
+    ),
+);
 ButtonMore.displayName = 'ButtonMore';
