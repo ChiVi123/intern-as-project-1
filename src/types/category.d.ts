09374107efe33836bc388b228ab82742baaa0601
ChiVi123 <nguyenhoangchivi@gmail.com>
2024-10-15 22:32:32 +0700

    delete root type category, location

--- src/types/category.d.ts
+++
@@ -1,6 +0,0 @@
-interface ICategoryModal {
-    id?: string;
-    path: string;
-    name: string;
-    isRoot?: boolean;
-}
--- src/types/location.d.ts
+++
@@ -1,9 +0,0 @@
-interface ILocationModal {
-    id?: string;
-    mark: string;
-    title: string;
-    content: string;
-    isList?: boolean;
-    category?: DocumentReference<ICategoryModal>;
-    position: { top: string; left: string };
-}
